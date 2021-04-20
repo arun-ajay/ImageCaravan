01f1b53e6adca9a808c1acd38250fe4b41cd0d57
nayma1 <79671237+nayma1@users.noreply.github.com>
2021-04-20 16:14:47 -0400

    Add files via upload

---
+++ server/ImageCaravanSchemas.sql
@@ -0,0 +1,51 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Followers (
+  userName TEXT,
+  follower TEXT
+);
+CREATE TABLE Following (
+  userName TEXT,
+  following TEXT
+);
+CREATE TABLE Hashtag (
+  hashtag TEXT,
+  visible BOOLEAN DEFAULT '1',
+  imageUUID INT
+);
+CREATE TABLE Image (
+  imageTitle TEXT,
+  imageUUID INT AUTO_INCREMENT,
+  imageCaption TEXT,
+  imageBase64 TEXT,
+  imageUploader TEXT,
+  visible BOOLEAN DEFAULT '1',
+  PRIMARY KEY (imageUUID)
+);
+CREATE TABLE ImageComments (
+  imageUUID INT NOT NULL,
+  comment TEXT,
+  commenter TEXT
+);
+CREATE TABLE ProfileComments (
+  imageUUID INT NOT NULL,
+  comment TEXT,
+  commenter TEXT
+);
+CREATE TABLE Users (
+  id INT AUTO_INCREMENT,
+  userName TEXT NOT NULL,
+  password TEXT NOT NULL,
+  bio text,
+  location text NOT NULL,
+  profilePicture TEXT,
+  imageList text,
+  PRIMARY KEY (id)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
